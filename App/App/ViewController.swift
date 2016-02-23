2a04d6ac64454f8eeee90fb75bd399500bda5de7
laijihua <laijihua@zuijiao.net>
2016-02-23 11:51:31 +0800

    first commit

---
+++ App/App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by laijihua on 16/2/23.
+//  Copyright © 2016年 laijihua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
